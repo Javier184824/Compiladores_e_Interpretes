bc62d236a43a5ca4d1e73a34a925d99049e66df3
Javier184824 <leejavier62@gmail.com>
2026-09-23 19:58:45 -0600

    Preprocesesador conectado con la función main del scanner

--- Proyecto_1/c.mk
+++ Proyecto_1/c.mk
@@ -2,8 +2,5 @@ SOURCE_DIR = c_sources
 
 SOURCES = $(wildcard $(SOURCE_DIR)/*.c)
 
-#output:
-#	flex -o c_sources/scanner.c flex_sources/scanner.l 
-#	gcc $(SOURCES) -o scanner
-output:
-	gcc c_sources/tokenizer.c c_sources/scanner.c -o scanner
+utput:
+	gcc $(SOURCES) -o tokenizer
